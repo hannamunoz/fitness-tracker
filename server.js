84f64dd5b4210553932e240de9e301eb53b44742
Hanna Munoz <hannamunoz@gmail.com>
2021-04-18 20:21:11 -0400

    deploying

--- server.js
+++ server.js
@@ -10,3 +10,21 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log(`Now listening on port ${PORT}`);
+});
